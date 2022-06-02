18dc6311b2e3ad59c8139d9ff9f88abac62ab712
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2022-06-02 18:49:44 +0200

    set dice state

--- src/componant/Main.js
+++ src/componant/Main.js
@@ -3,14 +3,17 @@ import React from "react";
 export default class Main extends React.PureComponent{
    
     // const [num,setNum] = React.useState(1);
-       state = { num: 1 };
-      add=()=>this.setState(state => {return {num : state.num+1}})
-    
+       state = { dice : [1,2,3,4,5,1,2,3,4] };
+        dice = this.state.dice.map(elem=><div className="grid-item">{elem}</div>);
+        
+       
       render(){
         return(
-           <div>
-              <h1 className="main"> <button onClick={this.add}>add </button>  {this.state.num} </h1>
-           </div> 
+           <main className="main">
+              <div className="grid-container">
+                {this.dice}
+              </div>
+           </main> 
         )
       }
        
