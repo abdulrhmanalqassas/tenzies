01548af644fbe66a4379122e9357f6426ffa62ca
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2022-06-02 19:17:47 +0200

    dice style

--- src/componant/Main.js
+++ src/componant/Main.js
@@ -3,8 +3,8 @@ import React from "react";
 export default class Main extends React.PureComponent{
    
     // const [num,setNum] = React.useState(1);
-       state = { dice : [1,2,3,4,5,1,2,3,4] };
-        dice = this.state.dice.map(elem=><div className="grid-item">{elem}</div>);
+       state = { dice : [1,2,3,4,5,6,1,2,3,4] };
+        dice = this.state.dice.map(elem=><div className="grid-item"><h1 className="grid-item-num">{elem}</h1></div>);
         
        
       render(){
