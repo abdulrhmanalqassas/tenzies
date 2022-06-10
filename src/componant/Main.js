977c0f0c38d72b2509f01288536147df71ec11fc
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2022-06-10 19:37:49 +0200

    holdDice in state & add style

--- src/componant/Main.js
+++ src/componant/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import Dice from "./Dice.js";
 import { nanoid } from "nanoid";
 
+
 export default class Main extends React.PureComponent {
   allNewDice = () => {
     let diceArray = [];
@@ -12,24 +13,18 @@ export default class Main extends React.PureComponent {
     return diceArray;
   };
 
-  state = { dice: this.allNewDice() };
-  // try = id=>{
-  //   this.state.dice.map(
-  //     die=>{if (die.id === id )
-  //       {die.isHeld = true }
-  //       else return die})
-  // };
-  holdDice = (id) => {
-    this.setState((state) => ({
-      dice: [
-        ...state.dice.map((valu) => {
-          if (valu.id === id) {
-            return { ...valu, isHeld: true };
-          } else return valu;
-        }),
-      ],
-    }));
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.tenzies===false){
+    console.log('Prev state', prevState.tenzies); // Before update
+    console.log('New state', this.state.tenzies); // After update 
+  }
+  }
+  state = { 
+    dice: this.allNewDice(),
+    tenzies: false
   };
+
+  
   curintDice = () => {
     let myarr = this.state.dice.map((value, index) => {
       return (
@@ -44,16 +39,42 @@ export default class Main extends React.PureComponent {
     });
     return myarr;
   };
+
+  holdDice = (id) => {
+    this.setState((state) => ({
+      dice: [
+        ...state.dice.map((valu) => {
+          if (valu.id === id) {
+            return { ...valu, isHeld: !valu.isHeld };
+          } else return valu;
+        }),
+      ],
+    }));
+  };
+
   roll = () => {
-    console.log("nice ass");
-    this.setState({ dice: this.allNewDice() });
-    console.log(this.state.dice);
+    this.setState((state)=>({
+      dice : [...state.dice.map((die)=>{
+        if(die.isHeld){
+          return die
+        }else {
+          let random = Math.floor(Math.random() * 6 + 1);
+          return({ value: random, isHeld: false, id: nanoid() })
+        }
+      })]
+    
+    }));
+   
   };
 
   render() {
     return (
       <main className="main">
-        {console.log("renderd")}
+        <h1 className="title">Tenzies</h1>
+            <p className="instructions">
+              Roll until all dice are the same. 
+              Click each die to freeze it at its current value between rolls.
+            </p>
         <div className="grid-container">{this.curintDice()}</div>
         <div onClick={this.roll} className="butt-roll">
           <h2 className="butt-roll-text">Roll</h2>
